0f56c8687b319e8c81157101b6b45407927fe97a
Spence Churchill <spence@duck.com>
2022-03-16 15:58:21 -0700

    implement regex into interpreter

--- docs/script.js
+++ docs/script.js
@@ -1,76 +1,31 @@
 // Lexer
-const lexer = str => str.split(' ').map(s => s.trim()).filter(s => s.length);
+const label_regex = new RegExp(/.*(?=:)/g);
+const operation_regex = new RegExp(/(?<=:\s*|^\s+)\S+/g);
+const operand_regex = new RegExp(/(?<!;\s*)((?<=(:*\s+\S+\s+))[^\s,;]+)/g);
+const comment_regex = new RegExp(/(?<=;).*/g);
 
-// Parser
-const Op = Symbol('op');
-const Num = Symbol('num');
-const parser = tokens => {
-    let c = 0;
+const parser = line => {
+    const label = line.match(label_regex)?.at(0);
+    const operation = line.match(operation_regex)?.at(0);
+    const operands = line.match(operand_regex);
+    const comment = line.match(comment_regex)?.at(0);
 
-    const peek = _ => tokens[c];
-    const consume = _ => tokens[c++];
-
-    const parseNum = _ => ({
-        val: parseInt(consume()),
-        type: Num
-    });
-
-    const parseOp = _ => {
-        const node = {
-            val: consume(),
-            type: Op,
-            expr: []
-        };
-        while (peek()) node.expr.push(parseExpr());
-        return node;
-    };
-
-    const parseExpr = _ => /\d/.test(peek()) ? parseNum() : parseOp();
-
-    return parseExpr();
-};
-
-// Transpiler
-const transpiler = ast => {
-    const opMap = {
-        mov: '=',
-        add: '+',
-        sub: '-',
-        div: '/',
-
-        inc: '++',
-        dec: '--',
-
-        and: '&',
-        or: '|',
-        xor: '^',
-        not: '~',
-    };
-    const transpileNode = ast => ast.type === Num ? transpileNum(ast) : transpileOp(ast);
-    const transpileNum = ast => ast.val;
-    const transpileOp = ast => `(${ast.expr.map(transpileNode).join(' ' + opMap[ast.val] + ' ')})`;
-    return transpileNode(ast);
-};
-
-const run = code => {
-    let output;
-    try {
-        output = eval(code);
-    } catch (e) {
-        output = e.message;
+    return {
+        label,
+        operation,
+        operands,
+        comment,
     }
-    return output;
 }
 
-document.getElementById("build").onclick = _ => {
-    const source = document.getElementById("source").value;
-    const tokens = lexer(source);
-    const parsed = parser(tokens);
-    const transpiled = transpiler(parsed);
-    const output = run(transpiled);
-
-    document.getElementById("lexerOutput").innerText = tokens;
-    document.getElementById("parserOutput").innerText = JSON.stringify(parsed);
-    document.getElementById("transpilerOutput").innerText = transpiled;
-    document.getElementById("output").innerText = output;
+document.getElementById("source").onkeyup = e => {
+    if (e.key === "Enter") {
+        const span = document.createElement("span");
+        const line = e.target.value;
+        const parsed = parser(line);
+        console.info(parsed);
+        span.innerText = line + "\n";
+        document.getElementById("output").appendChild(span);
+        e.target.value = "";
+    }
 }
