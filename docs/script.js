d0fab8066e44d9e9f2186997a08e28fe9b6752e4
Spence Churchill <spence@duck.com>
2022-03-16 23:28:19 -0700

    improve code runner

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,18 @@
+// Globals
+const source = document.getElementById("source");
+const run_btn = document.getElementById("run");
+let state = {};
+
 // Lexer
 let label_regex = new RegExp(/^\S*(?=:)/g);
 let operation_regex = new RegExp(/(?<!;.*)((?<=:\s*|^\s+)\S+)/g);
 let operand_regex = new RegExp(/(?<!;.*)((?<=(:*\s+\S+\s+))[^\s,;]+)/g);
 let comment_regex = new RegExp(/(?<=;).*/g);
 
+// Parser
 function parser(line) {
+    run_btn.innerText = "Parse";
+
     const label = line.match(label_regex)?.at(0);
     const operation = line.match(operation_regex)?.at(0);
     const operands = line.match(operand_regex);
@@ -16,6 +24,11 @@ function parser(line) {
         operands,
         comment,
     }
+}
+
+// Interpreter
+function interpreter(parsed) {
+    run_btn.innerText = "Interpret";
 }
 
 function appendCode(parsed) {
@@ -37,9 +50,9 @@ function appendCode(parsed) {
     comment_span.innerText = comment;
     newLine_span.innerText = newLine;
 
-    label_span.style.color = "#0000b5";
-    operation_span.style.color = "#b50000";
-    operands_span.style.color = "#005500";
+    label_span.style.color = "#000080";
+    operation_span.style.color = "#800000";
+    operands_span.style.color = "#008000";
     comment_span.style.color = "#808080";
 
     if (parsed.label)
@@ -53,12 +66,64 @@ function appendCode(parsed) {
     document.getElementById("output").appendChild(newLine_span);
 }
 
-document.getElementById("source").onkeyup = e => {
-    if (e.key === "Enter") {
-        const span = document.createElement("span");
-        const line = e.target.value;
-        const parsed = parser(line);
-        appendCode(parsed);
-        e.target.value = "";
+function resetTextArea(textArea) {
+    textArea.value = "";
+    textArea.rows = 1;
+}
+
+function run(lines) {
+    run_btn.disabled = true; // Disable button
+    const parsed_lines = [];
+    for (const line of lines.split("\n")) {
+        // Parse lines
+        parsed_lines.push(parser(line));
+        // Display lines in output
+        appendCode(parsed_lines?.at(parsed_lines.length - 1));
     }
+    for (const parsed of parsed_lines) {
+        // Interpret lines
+        interpreter(parsed);
+    }
+    run_btn.innerText = "Run";
+    run_btn.disabled = false; // Enable button
+}
+
+source.onkeydown = e => {
+    if (e.key === "Tab") {
+        // Prevent focus from moving to next element
+        e.preventDefault();
+        const start = source.selectionStart;
+        const end = source.selectionEnd;
+
+        // set textarea value to before+tab+after
+        source.value = source.value.substring(0, start) +
+            "\t" + source.value.substring(end);
+
+        // move to correct position
+        source.selectionStart = source.selectionEnd = start + 1;
+    }
+    if (e.key === "Enter") {
+        // Expand textarea on enter
+        e.target.rows++;
+    }
+    if (e.key === "Backspace") {
+        // Shrink textarea on backspace
+        if (e.target.rows > 1)
+            e.target.rows--;
+    }
+    if (e.shiftKey && e.key === "Enter") {
+        run(e.target.value);
+        // Reset textarea when code is run
+        resetTextArea(e.target);
+    }
+}
+
+source.onkeyup = e => {
+    if (e.shiftKey && e.key === "Enter")
+        resetTextArea(e.target);
+}
+
+run_btn.onclick = _ => {
+    run(source.value);
+    resetTextArea(source);
 }
