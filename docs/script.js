e638d9e2c17ca3ba1cdeb9989b01fa798009c207
Spence Churchill <spence@duck.com>
2022-03-18 13:33:09 -0700

    begin working on interpreter

--- docs/script.js
+++ docs/script.js
@@ -3,6 +3,29 @@ const source = document.getElementById("source");
 const run_btn = document.getElementById("run");
 let state = {};
 const tab = 4;
+const instructions = {
+    "add": "+",
+    "sub": "-",
+    "mul": "*",
+    "div": "/",
+    "and": "&",
+    "or": "|",
+    "xor": "^",
+    "beq": "==",
+    "bne": "!=",
+    "bgt": ">",
+    "blt": "<",
+    "brk": "break",
+    "jmp": "jump",
+    "lb": "=",
+    "lw": "=",
+    "sb": "=",
+    "sw": "=",
+    "sll": "<<",
+    "srl": ">>",
+    "sla": "<<",
+    "sra": ">>",
+};
 
 // Lexer
 let label_regex = new RegExp(/^\w+(?=:)/g);
@@ -14,6 +37,7 @@ let comment_regex = new RegExp(/;.*/g);
 function parser(line) {
     run_btn.innerText = "Parse";
 
+    // lex and parse with regex
     const label = line.match(label_regex)?.at(0);
     const operation = line.match(operation_regex)?.at(0).toLowerCase();
     const operands = line.match(operand_regex);
@@ -30,6 +54,59 @@ function parser(line) {
 // Interpreter
 function interpreter(parsed) {
     run_btn.innerText = "Interpret";
+
+    let line = "";
+    switch (parsed.operation) {
+        case "add":
+            line = parsed.operands?.at(0) + "+" + parsed.operands?.at(1);
+        case "sub":
+            line = parsed.operands?.at(0) + "-" + parsed.operands?.at(1);
+        case "mul":
+            line = parsed.operands?.at(0) + "*" + parsed.operands?.at(1);
+        case "div":
+            line = parsed.operands?.at(0) + "/" + parsed.operands?.at(1);
+        case "and":
+            line = parsed.operands?.at(0) + "&" + parsed.operands?.at(1);
+        case "or":
+            line = parsed.operands?.at(0) + "|" + parsed.operands?.at(1);
+        case "xor":
+            line = parsed.operands?.at(0) + "^" + parsed.operands?.at(1);
+        case "beq":
+            line = parsed.operands?.at(0) + "==" + parsed.operands?.at(1);
+        case "bne":
+            line = parsed.operands?.at(0) + "!=" + parsed.operands?.at(1);
+        case "bgt":
+            line = parsed.operands?.at(0) + ">" + parsed.operands?.at(1);
+        case "blt":
+            line = parsed.operands?.at(0) + "<" + parsed.operands?.at(1);
+        case "brk":
+            line = "break";
+        case "jmp":
+            line = "jump " + parsed.operands?.at(0);
+        case "lb":
+            line = parsed.operands?.at(0) + "=" + parsed.operands?.at(1);
+        case "lw":
+            line = parsed.operands?.at(0) + "=" + parsed.operands?.at(1);
+        case "sb":
+            line = parsed.operands?.at(0) + "=" + parsed.operands?.at(1);
+        case "sw":
+            line = parsed.operands?.at(0) + "=" + parsed.operands?.at(1);
+        case "sll":
+            line = parsed.operands?.at(0) + "<<" + parsed.operands?.at(1);
+        case "srl":
+            line = parsed.operands?.at(0) + ">>" + parsed.operands?.at(1);
+        case "sla":
+            line = parsed.operands?.at(0) + "<<" + parsed.operands?.at(1);
+        case "sra":
+            line = parsed.operands?.at(0) + ">>" + parsed.operands?.at(1);
+    }
+
+    try {
+        const result = eval(line);
+        console.log(result);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 function appendCode(parsed, line) {
@@ -66,6 +143,7 @@ function appendCode(parsed, line) {
     if (div.hasChildNodes())
         document.getElementById("output").appendChild(div);
     else if (line !== "")
+        // If parsing fails, display error as comment
         appendCode({
             label: "",
             operation: "",
